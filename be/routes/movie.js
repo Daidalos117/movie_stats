f21a27c0ac691098708b7f19542aaadf8179be2a
Roman Rajchert <rajchert.roman@gmail.com>
2020-01-12 19:38:46 +0100

    Grouping

--- be/routes/movie.js
+++ be/routes/movie.js
@@ -36,6 +36,7 @@ router.get('/', jwt, async function(req, res) {
       }
     }, // match, search
     { $unwind: '$movie' }, // movie is returned as array, so unwind makes it normal obj
+		{ $group: { _id: '$_id', watched_at: { "$first": "$watched_at" }, movie: { "$first": "$movie" }} },
     {
       $project: {
         // select only some fields
@@ -99,94 +100,93 @@ router.get('/', jwt, async function(req, res) {
   }
 });
 
-
 router.get('/sync', jwt, async function(req, res) {
-	const { user } = req;
+  const { user } = req;
 
-	async function fetch(params = {}) {
-		let response;
-		try {
-			response = await traktApi.get('sync/history/movies', {
-				headers: {
-					Authorization: `Bearer ${req.user.trakt.access_token}`
-				},
-				params
-			});
-		} catch (e) {
-			if (e) {
-				res.send(400, e);
-				return;
-			}
-		}
+  async function fetch(params = {}) {
+    let response;
+    try {
+      response = await traktApi.get('sync/history/movies', {
+        headers: {
+          Authorization: `Bearer ${req.user.trakt.access_token}`
+        },
+        params
+      });
+    } catch (e) {
+      if (e) {
+        res.send(400, e);
+        return;
+      }
+    }
 
-		if (response.data) {
-			const { data } = response;
-			//if(!data.length) res.response(204);
-			const newData = data.map(async history => {
-				let [errH, foundHistory] = await to(
-					HistoryModel.find({ watched_at: history.watched_at })
-				);
+    if (response.data) {
+      const { data } = response;
+      //if(!data.length) res.response(204);
+      const newData = data.map(async history => {
+        let [errH, foundHistory] = await to(
+          HistoryModel.find({ watched_at: history.watched_at })
+        );
 
-				if (foundHistory && foundHistory.length > 0) return;
-				const { movie, ...restHistory } = history;
-				let [errM, foundMovie] = await to(
-					MovieModel.find({ 'ids.trakt': movie.ids.trakt })
-				);
-				let movieId = foundMovie.length && foundMovie[0]._id;
+        if (foundHistory && foundHistory.length > 0) return;
+        const { movie, ...restHistory } = history;
+        let [errM, foundMovie] = await to(
+          MovieModel.find({ 'ids.trakt': movie.ids.trakt })
+        );
+        let movieId = foundMovie.length && foundMovie[0]._id;
 
-				if (!foundMovie.length) {
-					let newMovie = new MovieModel({ ...movie });
-					let [errSM, savedMovie] = await to(newMovie.save());
-					movieId = savedMovie._id;
-				}
+        if (!foundMovie.length) {
+          let newMovie = new MovieModel({ ...movie });
+          let [errSM, savedMovie] = await to(newMovie.save());
+          movieId = savedMovie._id;
+        }
 
-				console.log(movie.title);
-				console.table(foundMovie);
+        console.log(movie.title);
+        console.table(foundMovie);
 
-				const newHistory = new HistoryModel({
-					...restHistory,
-					movie: movieId,
-					user: user._id
-				});
+        const newHistory = new HistoryModel({
+          ...restHistory,
+          movie: movieId,
+          user: user._id
+        });
 
-				let [errNH, savedHistory] = await to(newHistory.save());
-				return savedHistory;
-			});
+        let [errNH, savedHistory] = await to(newHistory.save());
+        return savedHistory;
+      });
 
-			Promise.all(newData).then(() => {
-				res.send(200, newData);
-			});
-		}
-	}
+      Promise.all(newData).then(() => {
+        res.send(200, newData);
+      });
+    }
+  }
 
-	const [errorH, historyFind] = await to(
-		HistoryModel.findOne({ user: user._id }).sort({ watched_at: -1 })
-	);
+  const [errorH, historyFind] = await to(
+    HistoryModel.findOne({ user: user._id }).sort({ watched_at: -1 })
+  );
 
-	const params = {};
+  const params = {};
 
-	if (historyFind) {
-		params.start_at = historyFind.watched_at;
-		fetch(params);
-	} else {
-		//  initial fetch
-		traktApi
-			.get('sync/history/movies', {
-				headers: {
-					Authorization: `Bearer ${req.user.trakt.access_token}`
-				},
-				params: {
-					limit: 1
-				}
-			})
-			.then(iRes => {
-				const { headers } = iRes;
-				const countIntems =
-					headers['X-Pagination-Item-Count'] ||
-					headers['x-pagination-item-count'];
-				fetch({ ...params, limit: countIntems });
-			});
-	}
+  if (historyFind) {
+    params.start_at = historyFind.watched_at;
+    fetch(params);
+  } else {
+    //  initial fetch
+    traktApi
+      .get('sync/history/movies', {
+        headers: {
+          Authorization: `Bearer ${req.user.trakt.access_token}`
+        },
+        params: {
+          limit: 1
+        }
+      })
+      .then(iRes => {
+        const { headers } = iRes;
+        const countIntems =
+          headers['X-Pagination-Item-Count'] ||
+          headers['x-pagination-item-count'];
+        fetch({ ...params, limit: countIntems });
+      });
+  }
 });
 
 router.get('/:id', jwt, async function(req, res) {
@@ -234,6 +234,5 @@ router.get('/:id', jwt, async function(req, res) {
     res.send(404);
   }
 });
-
 
 module.exports = router;
