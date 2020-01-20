7f5c2a1366d0599b77ee36f2c61a947ca569da9f
Roman Rajchert <rajchert.roman@gmail.com>
2020-01-20 20:14:25 +0100

    Fixed app kicking

--- fe/src/api/backend.ts
+++ fe/src/api/backend.ts
@@ -7,14 +7,16 @@ const instance = axios.create({
 });
 
 export const fetcher = (url: string, config = {}) => {
-  return instance(url, { ...config }).then(response => {
-    // returning the data here allows the caller to get it through another .then(...)
-    return response.data
-  });
+  return instance(url, { ...config })
+    .then(response => {
+      // returning the data here allows the caller to get it through another .then(...)
+      return response.data;
+    })
+    .catch(e => {
+      console.error('Fetch error', e);
+    });
 };
 
 instance.defaults.timeout = 10000;
-
-
 
 export default instance;
