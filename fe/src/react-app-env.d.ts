63055125021dce57dfda7b073c89c9062059f865
Roman Rajchert <rajchert.roman@gmail.com>
2020-02-15 18:10:11 +0100

    Fix type of env variable

--- fe/src/react-app-env.d.ts
+++ fe/src/react-app-env.d.ts
@@ -4,6 +4,6 @@ declare namespace NodeJS {
     PUBLIC_URL: string,
     REACT_APP_TMDB_API_KEY: string,
     REACT_APP_TMDB_TOKEN: string,
-    REACT_APP_ITEMS_PER_PAGE_SYNC: number;
+    REACT_APP_ITEMS_PER_PAGE_SYNC: string;
   }
 }
